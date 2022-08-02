e9c027e662b3230bc20a774d871e11ce8078c8fc
sunab <100410283+ranjitkar@users.noreply.github.com>
2022-08-02 20:40:33 +0545

    test

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const helmet = require("helmet");
+const app = express();
+app.set('view engine', 'pug')
+app.use(helmet());
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/pug', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
